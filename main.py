8af108ebfac8d93c818066e59bf1cbb81c67812f
luisangel <zS16012135@estudiantes.uv.mx>
2020-06-17 21:37:10 -0500

    Se concluyó el código de la búsqueda de costo uniforme.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from modules import search as sc
+import time
 
 print('Ingrese la letra correspondiente a la ciudad de inicio ')
-n = str(input())
+start = str(input())
+print('Ingrese la letra correspondiente a la ciudad de llegada ')
+end = str(input())
 
-search_cost = sc.busqueda(sc.map_city, 'F', 'B')
-print(search_cost)
+start_time = time.time()
+search_cost = sc.busqueda(sc.map_city, start, end)
+end_time = time.time()
+all_time = round(end_time - start_time, 1000)
+
+print('\nPLa ruta de la búsqueda de costo uniforme es: ', search_cost)
+print('El tiempo que tardó en realizar la búsqueda es : ', all_time)
--- modules/search.py
+++ modules/search.py
@@ -5,38 +5,40 @@ tree = []
 travel = []
 
 
-def busqueda(mapa, start, end) -> List[float]:
+def busqueda(mapa, start: str, end: str) -> List[str]:
     heapq.heappush(tree, start)
+    travel.append(start)
     while len(tree) != 0:
         city = heapq.heappop(tree)
-        if start == end:
-            return end
-        for vecino in mapa[city]:
-            if vecino not in travel:
-                heapq.heappush(tree, vecino)
-                travel.append(vecino)
+        if city == end:
+            return travel
+        for neighbor in mapa[city]:
+            if neighbor[1] not in travel:
+                heapq.heappush(tree, neighbor[1])
+                travel.append(neighbor[1])
+                break
 
 
 # Usando la tabla de ciudades proporcionada por la mtra paty :)
 map_city = {
-    'A': [(95, 'S'), (52, 'M'), (100, 'N')],
-    'B': [(114, 'X'), (61, 'V'), (62, 'J'), (49, 'Z')],
-    'C': [(43, 'D'), (22, 'M'), (169, 'S')],
+    'A': [(52, 'M'), (95, 'S'), (180, 'N')],
+    'B': [(49, 'Z'), (61, 'V'), (62, 'J'), (114, 'X')],
+    'C': [(22, 'M'), (43, 'D'), (169, 'S')],
     'D': [(43, 'C')],
-    'E': [(208, 'F'), (138, 'O')],
-    'F': [(43, 'H'), (25, 'Y'), (208, 'E')],
-    'G': [(116, 'Z'), (180, 'X'), (75, 'P'), (40, 'T')],
+    'E': [(138, 'O'), (208, 'F')],
+    'F': [(25, 'Y'), (43, 'H'), (208, 'E')],
+    'G': [(40, 'T'), (75, 'P'), (116, 'Z'), (180, 'X')],
     'H': [(43, 'F'), (120, 'X')],
-    'J': [(62, 'B'), (44, 'Y'), (78, 'O')],
+    'J': [(44, 'Y'), (62, 'B'), (78, 'O')],
     'M': [(22, 'C'), (52, 'A')],
     'N': [(95, 'O'), (180, 'A')],
-    'O': [(95, 'N'), (78, 'J'), (120, 'V'), (138, 'E')],
-    'P': [(75, 'G'), (43, 'T'), (119, 'U')],
-    'S': [(95, 'A'), (84, 'V'), (169, 'C')],
-    'T': [(43, 'P'), (40, 'G')],
+    'O': [(78, 'J'), (95, 'N'), (120, 'V'), (138, 'E')],
+    'P': [(43, 'T'), (75, 'G'), (119, 'U')],
+    'S': [(84, 'V'), (95, 'A'), (169, 'C')],
+    'T': [(40, 'G'), (43, 'P')],
     'U': [(104, 'X'), (119, 'P')],
-    'V': [(84, 'S'), (61, 'B'), (120, 'O')],
-    'X': [(104, 'U'), (64, 'Z'), (114, 'B'), (120, 'H'), (180, 'G')],
+    'V': [(61, 'B'), (84, 'S'), (120, 'O')],
+    'X': [(64, 'Z'), (104, 'U'), (114, 'B'), (120, 'H'), (180, 'G')],
     'Y': [(25, 'F'), (44, 'J')],
-    'Z': [(64, 'X'), (49, 'B'), (116, 'G')]
+    'Z': [(49, 'B'), (64, 'X'), (116, 'G')]
 }
